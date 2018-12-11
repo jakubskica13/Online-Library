cbc27fb947152e44f68a4b4318a46b0437ce99e7
jakubskica13 <41347959+jakubskica13@users.noreply.github.com>
2018-12-10 19:05:03 -0500

    Add files via upload

---
+++ Online-Libary/inc/data.php
@@ -0,0 +1,181 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Fahrenheit 451",
+	"img" => "img/media/book1.jpg",
+    "genre" => "Tech",
+    "format" => "Paperback",
+    "year" => 1994,
+    "category" => "Books",
+    "authors" => [
+        "Ray Bradbury",
+
+    ],
+    "publisher" => "Simon & Schuster",
+    "isbn" => '978-1982102609'
+];
+$catalog[102] = [
+    "title" => "The Great Gatsby",
+    "img" => "img/media/book2.jpg",
+    "genre" => "Crime and Drama",
+    "format" => "paperback",
+    "year" => 2004,
+    "category" => "Books",
+    "authors" => [
+        "F.Scott Fitzgerald"
+    ],
+    "publisher" => "Scribner",
+    "isbn" => '978-0743273273565'
+];
+$catalog[103] = [
+    "title" => "12 Angry Men",
+    "img" => "img/media/book3.jpg",
+    "genre" => "Crime and Drama",
+    "format" => "paperback",
+    "year" => 1954,
+    "category" => "Books",
+    "authors" => [
+        "Reginald Rose"
+    ],
+    "publisher" => "Dramatic Pub Co",
+    "isbn" => '087129401x'
+];
+$catalog[104] = [
+    "title" => "The Hunger Games",
+    "img" => "img/media/book4.jpg",
+    "genre" => "Dystopian,adventure,sience fiction,drama,action",
+    "format" => "Paperback",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Suzanne Collins"
+    ],
+    "publisher" => "Scholastic,Incorporated",
+    "isbn" => '0545425115'
+];
+//Movies
+$catalog[201] = [
+    "title" => "The Amazing Spider-Man 2",
+    "img" => "img/media/movie1.jpg",
+    "genre" => "Action,Adventure,Drama, Science Fiction & Fanstasy",
+    "format" => "DVD",
+    "year" => 2014,
+    "category" => "Movies",
+    "director" => "Marc Webb",
+    "writers" => [
+        "Robert Orci",
+				"Alex Kurtzman",
+        "Jeff Pinkner"
+    ],
+    "stars" => [
+        "Andrew Garfield",
+        "Emma Stone",
+        "Dane Dehaan",
+        "James Foxx",
+				"Chris Cooper"
+				"Paul Giamatti"
+    ]
+];
+$catalog[202] = [
+    "title" => "Assassins Creed",
+    "img" => "img/media/movie2.jpg",
+    "genre" => "Fantasy,Science Fiction,Action,Adventure,Drama",
+    "format" => "DVD",
+    "year" => 2016,
+    "category" => "Movies",
+    "director" => "Justin Kurzel",
+    "writers" => [
+        "Bill Collage",
+				"Micheal Lesslie"
+    ],
+    "stars" => [
+        "Micheal Fassbender",
+        "Marion Cotillard",
+        "Jeremy Irons",
+        "Brendan Gleeson",
+        "Charlotte Rampling",
+        "Micheal Kenneth Williams"
+    ]
+];
+$catalog[203] = [
+    "title" => "Neighbors 2: Soronity Rising",
+    "img" => "img/media/movie3.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2016,
+    "category" => "Movies",
+    "director" => "Nicholas",
+    "writers" => [
+        "Seth Rogen",
+        "Evan Goldberg",
+        "Brendan O'Brien",
+        "Micholes Stoller",
+				"Andrew Jay Cohen"
+    ],
+    "stars" => [
+        "Seth Rogen",
+        "Zac Efron",
+        "Rose Byrne",
+        "Chloë Grace Moretz",
+        "Kiersey Clemons",
+        "Selena Gomez"
+    ]
+];
+$catalog[204] = [
+    "title" => "Scream 4",
+    "img" => "img/media/movie4.jpg",
+    "genre" => "Horror,Mystery,and Suspense",
+    "format" => "DVD",
+    "year" => 2011,
+    "category" => "Movies",
+    "director" => "Wes Craven",
+    "writers" => [
+        "Kevin Williamson"
+    ],
+    "stars" => [
+        "Neve Campbell",
+        "Courtney Cox",
+        "David Arquette",
+        "Emma Roberts",
+        "Rory Culkin",
+        "Hayden Panettiere"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "RED",
+    "img" => "img/media/music1.jpg",
+    "genre" => "Country",
+    "format" => "CD",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Taylor Swift"
+];
+$catalog[302] = [
+    "title" => "No Fences",
+    "img" => "img/media/music33.jpg",
+    "genre" => "Country",
+    "format" => "Cassette",
+    "year" => 1990,
+    "category" => "Music",
+    "artist" => "Garth Brooks"
+];
+$catalog[303] = [
+    "title" => "Stars Dance",
+    "img" => "img/media/music3.jpg",
+    "genre" => "Pop",
+    "format" => "CD",
+    "year" => 2013,
+    "category" => "Music",
+    "artist" => "Selena Gomez"
+];
+$catalog[304] = [
+    "title" => "El Dorado",
+    "img" => "img/media/music4.jpg",
+    "genre" => "Latin Pop",
+    "format" => "CD",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Shakira"
+];
